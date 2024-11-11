f49cfd63613131dc4a448b994d154ccd7f59ec07
jaymhorsh <jaji.moshood1297@gmail.com>
2024-11-11 16:55:36 +0100

    updated code

---
+++ Desktop/React_2023/chainfren_tv/src/app/(auth)/register/confirmaccount/createAccount.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CreateAccount = () => {
+  return (
+    <div>
+      <h1>Account Confirmation</h1>
+      <p>Your account has been created successfully!</p>
+      {/* Additional confirmation details can be added here */}
+    </div>
+  );
+};
+
+export default CreateAccount;
